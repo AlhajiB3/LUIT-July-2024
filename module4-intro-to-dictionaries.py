985a51f94af2065c94d19a13b475c85e262b99c2
AlhajiB3 <133896864+AlhajiB3@users.noreply.github.com>
2024-07-31 14:41:44 -0400

    Introduction to dictionaires

---
+++ module4-intro-to-dictionaries.py
@@ -0,0 +1,15 @@
+emails = {
+    'Alhajib': 'alhajib23@yahoo.com',
+    'bAlhaji': 'bahacloud92@hotmail.com',
+    'Alihaji': 'ayegetmoney808@gmail.com'
+}
+print(emails['Alhajib'])
+
+spanish_animals = {
+    'dog': 'el perro',
+    'cat': 'el goto', 
+    'horse': 'el caballo',
+    'bird': 'el pajaro'
+}
+print(spanish_animals['dog'])
+print(spanish_animals)
