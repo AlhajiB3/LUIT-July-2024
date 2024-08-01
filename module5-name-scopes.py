113ee59d1d48612dbcccaf4df4861d0c2fb94d82
AlhajiB3 <133896864+AlhajiB3@users.noreply.github.com>
2024-08-01 13:17:49 -0400

    Introduction to Name Scopes

---
+++ module5-name-scopes.py
@@ -0,0 +1,19 @@
+def show_truth():
+    global mysterious_var
+    mysterious_var = 'New Surprise!'
+    print(mysterious_var)
+
+mysterious_var='Surprise!'
+print(mysterious_var)
+show_truth()
+print(mysterious_var)
+
+def show_truth():
+    global mysterious_var
+    mysterious_var.append('New Surprise!')
+    print(mysterious_var)
+
+mysterious_var = ['Surprise!']
+print(mysterious_var)
+show_truth()
+print(mysterious_var)
