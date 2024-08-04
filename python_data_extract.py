bb76dbb1c4bd7fec595e8c5a30cf849e183f736c
AlhajiB3 <133896864+AlhajiB3@users.noreply.github.com>
2024-08-04 19:46:03 -0400

    Extracting Data with Python

--- python_data_extract.py
+++ python_data_extract.py
@@ -1,5 +1,26 @@
+# Import the operating system
 import os
 
+# Define the directory to be scanned
 current_directory = os.getcwd()
 
+# Create the empty list to store file information
 Files = []
+
+# Iterate over all items in the directory
+for file in os.listdir(current_directory):
+    # Provide the full file path
+    file_path = os.path.join(current_directory, file)
+     # Verify that it is a file
+    if os.path.isfile(file_path):
+       # Get the file size
+        file_size = os.path.getsize(file_path)
+         # Create a dictionary with both the file and the size
+        Data = {'path': file_path, 'size': file_size}
+         # Append the dictionary to the list
+        Files.append(Data)
+
+
+# Print the file paths and sizes
+for file_info in Files:
+    print(f"File Path: {file_info['path']} | Size: {file_info['size']} bytes")
