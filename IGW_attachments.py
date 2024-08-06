e749fad8b351a66b9cfbcf8627ed7bc6d03d4ef2
AlhajiB3 <bahacloud92@gmail.com>
2024-08-06 16:33:38 -0400

    Internet Gateway Attachments

---
+++ IGW_attachments.py
@@ -0,0 +1,11 @@
+import boto3
+
+internet_gateway_id = 'igw-027598e082ee5b000'
+vpc_id = 'vpc-07f2b356093629517'
+
+ec2 = boto3.client('ec2')
+
+ec2.attach_internet_gateway(
+    InternetGatewayId=internet_gateway_id,
+    VpcId=vpc_id,
+)
