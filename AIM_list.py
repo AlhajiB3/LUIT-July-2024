b0c715ce5b07f3e7c3de18b4251a7608b9673a44
AlhajiB3 <bahacloud92@gmail.com>
2024-08-07 13:42:39 -0400

    Updated AMI

--- AIM_list.py
+++ AIM_list.py
@@ -5,10 +5,22 @@ ec2 = boto3.client('ec2')
 
 # Specify instance ID and name for new AMI
 instance_id = 'i-0dec7ea677be5e62a'
-ami_name = 'my-ami'
+ami_name = 'my-ami3'
+Image_Ids = 'ami-0a7347fd1793b97f0'
+Owners = '590183779739'
 
 # Create AMI
-response = ec2.create_image(InstanceId=instance_id, Name=ami_name)
+# response = ec2.create_image(InstanceId=instance_id, Name=ami_name)
+
+# List AMIs 
+# response = ec2.describe_images(ImageIds=Image_Ids, Owners=Owners)
+response = ec2.describe_images
 
 # Print AMI ID
 print(response['ImageId'])
+
+# Print Line Break
+print("Line break")
+
+# Print Describe Images
+print(response['ImageId'], ['Owners'])
