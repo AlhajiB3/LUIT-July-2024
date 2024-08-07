cbcf50a4f1a03fed8d0141c0a8cb6fe6a636003f
AlhajiB3 <bahacloud92@gmail.com>
2024-08-07 15:02:27 -0400

    Testing txt file

--- test_txt.py
+++ test_txt.py
@@ -6,3 +6,5 @@ s3 = boto3.client('s3')
 #     s3.put_object(Bucket="my-new-bucket-name-2024", Key="text.py", Body=f, ContentType="text/plain")
 
 s3.upload_file('text.py', "my-new-bucket-name-2024", "test_upload.py", ExtraArgs={'ContentType':'text/plain'})
+
+print('hi)')
